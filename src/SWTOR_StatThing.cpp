eb10e18bb00c77201cb02b38027c47776e642d79
Robin de Gier <de.gier.robin@gmail.com>
2015-06-11 10:50:14 +0200

    Added project.

---
+++ src/SWTOR_StatThing.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : SWTOR_StatThing.cpp
+// Author      : Whojoo
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <json/json.h>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "!!!Hello World!_!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
